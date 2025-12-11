c277fcfb56dfc979fcb8f6e27088ab48e779173d
hastalavic <javierjourney@gmail.com>
2025-12-11 23:41:18 +0800

    auto

--- app/_components/layout/Header.tsx
+++ app/_components/layout/Header.tsx
@@ -2,7 +2,7 @@ import NavBarPages from "@/app/_components/widgets/NavBarPages";
 
 export default function Header() {
   return (
-    <header className="w-full py-3 shadow-md bg-white/70 dark:bg-black/40 backdrop-blur-md z-40">
+    <header className="w-full py-3 shadow-md bg-white dark:bg-black z-40">
       <div className="max-w-6xl mx-auto flex items-center justify-between px-4">
         {/* 左側：Logo / 標題 */}
         <h1 className="text-2xl font-semibold">MBRI</h1>
