a3f864a0ad82919f568b9d532ab9aa4d24942f58
hastalavic <javierjourney@gmail.com>
2026-01-09 21:08:21 +0800

    update

--- app/design/widgets/NavBarPages.tsx
+++ app/design/widgets/NavBarPages.tsx
@@ -9,10 +9,10 @@ export default function Navbar() {
   const links = [
     { href: "/", label: "首頁" },
     { href: "/PSP", label: "生理狀態設置" },    
-    { href: "/MBRMonitor", label: "代謝監視器" },
-    { href: "/intake_entry", label: "攝取預覽" },
-    { href: "/AllElementsPage", label: "元素導覽" },
-    { href: "/IntakesPage", label: "攝取物列表" },
+    // { href: "/MBRMonitor", label: "代謝監視器" },
+    // { href: "/intake_entry", label: "攝取預覽" },
+    // { href: "/AllElementsPage", label: "元素導覽" },
+    // { href: "/IntakesPage", label: "攝取物列表" },
     { href: "/bvt_001", label: "BVT" },
 
   ];
